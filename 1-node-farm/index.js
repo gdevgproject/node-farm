d4e32bb3dfa5d5e8700b784fa026b6c8d4fca50d
gdevgproject <gdev.gproject@gmail.com>
2024-12-03 02:14:09 +0700

    Filling the Templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,10 +3,40 @@ import { fileURLToPath } from "url";
 import path from "path";
 import { readFile, readFileSync } from "fs";
 
+// SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
 const __dirname = (relativePath) =>
   path.join(path.dirname(fileURLToPath(import.meta.url)), relativePath);
 
-const data = readFileSync(__dirname("dev-data/data.json"), "utf-8");
+const tempOverview = readFileSync(
+  __dirname("./templates/template-overview.html"),
+  "utf-8"
+);
+const tempCard = readFileSync(
+  __dirname("./templates/template-card.html"),
+  "utf-8"
+);
+const tempProduct = readFileSync(
+  __dirname("./templates/template-product.html"),
+  "utf-8"
+);
+
+const data = readFileSync(__dirname("./dev-data/data.json"), "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = createServer((req, res) => {
@@ -14,7 +44,14 @@ const server = createServer((req, res) => {
 
   // Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
     // Product page
   } else if (pathName === "/product") {
